59e4ff36ffd7d3845a86584d48cb2d1f35b7114c
alchermd <johnalcherdoloiras@gmail.com>
2017-05-02 18:06:22 +0800

    Implement pay method
    The pay method allows an account to pay an amount that will  reduce the interest or balance of that account.
    
    The updating of interest in update_balance method is abstracted into its own method.
    
    A separate variable og_amount is created to reference the original amount borrowed by the account.

--- modules/Account.py
+++ modules/Account.py
@@ -6,31 +6,41 @@ class Account:
     ''' Class used for Account objects. '''
     balance = 0
     interest = 0
+    og_amount = 0
 
     def __init__(self, name, base_amount, start_date, int_rate):
         self.name = name
-        self.base_amount = base_amount
+        self.og_amount = self.base_amount = base_amount
         self.start_date = start_date
         self.int_rate = int_rate * 0.01
+
+        self.update_interest()
         self.update_balance()
 
     def show_info(self):
         ''' Prints the account information. '''
-        # Update the balance to ensure accurate data
-        self.update_balance()
-
         print("Account: {}".format(self.name))
         print("Opened: {}".format(self.start_date))
-        print("Statement: {} for {}% interest rate weekly.".format(self.base_amount, self.int_rate))
+        print("Statement: {} for {}% interest rate weekly.".format(self.og_amount, self.int_rate))
         print("Interest: {}".format(self.interest))
         print("Current balance: {}".format(self.balance))
 
     def update_balance(self):
-        ''' Calculates the balance of the account.
+        ''' Calculates the balance of the account. '''
+        self.balance = self.base_amount + self.interest
 
-            interest = base amount X interest rate x weeks due
-            balance = base amount + interest
-        '''
+    def update_interest(self):
+        ''' Calculates and updated the interest of the account. '''
         weeks_due = (datetime.date.today() - self.start_date).days // 7
         self.interest = self.base_amount * self.int_rate * weeks_due
-        self.balance = self.base_amount + self.interest
+
+    def pay(self, amount):
+        ''' Reduces the account balance by amount an amount '''
+        if amount > self.interest:
+            self.interest, overflow = 0, self.interest - amount
+            self.base_amount += overflow
+        else:
+            self.interest -= amount
+
+        self.update_balance()
+        

4d013ecf3711bffd298e490cc502f0cc1e07fece
alchermd <johnalcherdoloiras@gmail.com>
2017-05-02 18:08:44 +0800

    Demo the pay method.
    Comments are also added to visually group the methods called.

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ from modules.Account import Account
 
 def main():
     ''' Main function '''
+    # Create an account for 5000
     sample = Account("John Doe", 5000, date(2017, 3, 30), 5)
+    sample.show_info()
+
+    # Pay 5000 to base amount + interest
+    sample.pay(5000)
+    print()
     sample.show_info()
 
 main()

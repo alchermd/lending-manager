493835db96bd111b4dfd00b35a79f0a7d62b44e4
alchermd <johnalcherdoloiras@gmail.com>
2017-05-02 16:16:55 +0800

    Added update_balance method.
    update_balance calculates the total balance of the account from start_date up to today.
    
    update_balance is also ran in the constructor and the show_info method.

--- modules/Account.py
+++ modules/Account.py
@@ -1,5 +1,7 @@
 ''' Account module used for creating accounts. '''
 
+import datetime
+
 class Account:
     ''' Class used for Account objects. '''
     balance = 0
@@ -7,10 +9,19 @@ class Account:
         self.name = name
         self.base_amount = base_amount
         self.start_date = start_date
-        self.interest = interest
-
+        self.interest = interest * 0.01
+        self.update_balance()
 
     def show_info(self):
         ''' Prints the account information. '''
+        # Update the balance to ensure accurate data
+        self.update_balance()
+
         print("Account: {}".format(self.name))
-        print("Borrowed {} for {}% at {}.".format(self.base_amount, self.interest, self.start_date))
+        print("Borrowed {} for {}% interest at {}.".format(self.base_amount, self.interest, self.start_date))
+        print("Current balance: {}".format(self.balance))
+
+    def update_balance(self):
+        ''' Calculates the balance of the account. '''
+        weeks_due = (datetime.date.today() - self.start_date).days // 7
+        self.balance = self.base_amount * self.interest * weeks_due

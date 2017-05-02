b67076e6df998d2aa7008fd98bfa9da125f0f993
alchermd <johnalcherdoloiras@gmail.com>
2017-05-02 16:46:14 +0800

    Properly shows the interest and balance.
    interest should be a separate variable and balance is calculated as base amount + interest.
    
    show_info now prints a much cleaner summary of an account.

--- modules/Account.py
+++ modules/Account.py
@@ -5,11 +5,13 @@ import datetime
 class Account:
     ''' Class used for Account objects. '''
     balance = 0
-    def __init__(self, name, base_amount, start_date, interest):
+    interest = 0
+
+    def __init__(self, name, base_amount, start_date, int_rate):
         self.name = name
         self.base_amount = base_amount
         self.start_date = start_date
-        self.interest = interest * 0.01
+        self.int_rate = int_rate * 0.01
         self.update_balance()
 
     def show_info(self):
@@ -18,10 +20,17 @@ class Account:
         self.update_balance()
 
         print("Account: {}".format(self.name))
-        print("Borrowed {} for {}% interest at {}.".format(self.base_amount, self.interest, self.start_date))
+        print("Opened: {}".format(self.start_date))
+        print("Statement: {} for {}% interest rate weekly.".format(self.base_amount, self.int_rate))
+        print("Interest: {}".format(self.interest))
         print("Current balance: {}".format(self.balance))
 
     def update_balance(self):
-        ''' Calculates the balance of the account. '''
+        ''' Calculates the balance of the account.
+
+            interest = base amount X interest rate x weeks due
+            balance = base amount + interest
+        '''
         weeks_due = (datetime.date.today() - self.start_date).days // 7
-        self.balance = self.base_amount * self.interest * weeks_due
+        self.interest = self.base_amount * self.int_rate * weeks_due
+        self.balance = self.base_amount + self.interest

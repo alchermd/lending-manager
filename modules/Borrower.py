d83b4713c60cd911d571a777ccc61f5f8fb629dc
alchermd <johnalcherdoloiras@gmail.com>
2017-05-03 16:25:13 +0800

    Create Borrower class module
    The Borrower class handles the person who is borrowing money. The borrower can open multiple accounts if he/she has a good credit status.

---
+++ modules/Borrower.py
@@ -0,0 +1,35 @@
+''' Borrower module used for creating borrower objects. '''
+from modules.Account import Account
+
+class Borrower:
+    ''' Borrower class creates a borrower object that can hold multiple accounts. '''
+    accounts = []
+
+    def __init__(self, name, credit_status=True):
+        self.name = name
+        self.credit_status = credit_status
+
+    def open_account(self, base_amount, start_date, int_rate):
+        ''' Adds an account object to the borrower's accounts list. '''
+        if self.credit_status:
+            self.accounts.append(Account(self.name, base_amount, start_date, int_rate))
+        else:
+            print("Bad credit status")
+
+    def show_credits(self, index=-1):
+        ''' Shows the account information of the given index.
+            Shows an error message if no accounts are opened.
+            Shows all the accounts otherwise.
+        '''
+        if index < 0:
+            for account in self.accounts:
+                account.show_info()
+        elif not self.accounts:
+            print("No accounts opened")
+        else:
+            self.accounts[index].show_info()
+
+    def pay(self, index, amount):
+        ''' Pays an amount to the account at index. '''
+        self.accounts[index].pay(amount)
+        

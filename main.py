866abb56b6904b63b9fecf2e9c23ab136096d858
alchermd <johnalcherdoloiras@gmail.com>
2017-05-04 18:53:03 +0800

    Start to implement the interface loop.
    The interface loop prompts the user to enter a command that controls the flow of the program. The user can now quit the program succesfully.
    
    Create a "database" dictionary to hold the user's accounts.
    
    clear_delay function calls changed to 1 second instead of 1.5
    
    Fixed the logic for determining if the user is a new customer or not.

--- main.py
+++ main.py
@@ -14,27 +14,45 @@ def clear_delay(secs):
 
 def main():
     ''' The main function that allows the user to interact with the rest of the program. '''
+    # Setup
     os.system("clear")
+    database = {}
 
     # Greet the user.
     print("Good day! Welcome to our lending company!")
-    clear_delay(1.5)
+    clear_delay(1)
 
     # Prompt the user if they are a new customer.
-    print("Are you a returning customer? (Y/N) ", end='')
+    print("Are you a new customer? (Y/N) ", end='')
     customer_is_new = input().upper() == 'Y'
-    clear_delay(1.5)
+    clear_delay(1)
 
     # Ask for their name.
     print("What is your name? ", end='')
     name = input()
-    clear_delay(1.5)
+    clear_delay(1)
 
     if customer_is_new:
-        print("Let's open your first account, {}".format(name))
-        clear_delay(1.5)
+        database[name] = Borrower(name)
+        print("You are now registered to our services, {}".format(name))
+        clear_delay(1)
     else:
         print("Welcome back, {}".format(name))
-        clear_delay(1.5)
+        clear_delay(1)
+
+    # Interface loop.
+    choice = None
+    while choice != 'Q':
+        print("How may we help you?")
+        print("[0]: Create a new account on my name.")
+        print("[1]: Check accounts under my name.")
+        print("[2]: Pay an existing account.")
+        print("[Q]: Exit the program.")
+        print("\nI want to: ", end='')
+
+        choice = input().upper()
+        clear_delay(1)
+
+    print("Thank you for using our services. See you soon!")
 
 main()

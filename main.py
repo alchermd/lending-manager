92278430902a75ede36103f301421e08b1b37a17
alchermd <johnalcherdoloiras@gmail.com>
2017-05-04 18:02:31 +0800

    Overhaul the main program as an interface rather than a simple demo of methods.
    The main program will run as a simple command line interface where it prompts the user for information. It will also allow the user to interact with the program by giving control options (such as "enter 1 to open account").
    
    A helper function clear_delay is implemented.
    
    The program prompts the user if they are a new customer and prints a message accordingly.

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 #!/usr/bin/python3.5
 ''' Main program '''
 
+import os
+import time
 from datetime import date
-from modules.Account import Account
+from modules.Borrower import Borrower
+
+def clear_delay(secs):
+    ''' Delays execution of the program and then clears the screen afterwards. '''
+    time.sleep(secs)
+    os.system("clear")
 
 
 def main():
-    ''' Main function '''
-    # Create an account for 5000
-    sample = Account("John Doe", 5000, date(2017, 3, 30), 5)
-    sample.show_info()
+    ''' The main function that allows the user to interact with the rest of the program. '''
+    os.system("clear")
 
-    # Pay 5000 to base amount + interest
-    sample.pay(5000)
-    print()
-    sample.show_info()
+    # Greet the user.
+    print("Good day! Welcome to our lending company!")
+    clear_delay(1.5)
+
+    # Prompt the user if they are a new customer.
+    print("Are you a returning customer? (Y/N) ", end='')
+    customer_is_new = input().upper() == 'Y'
+    clear_delay(1.5)
+
+    # Ask for their name.
+    print("What is your name? ", end='')
+    name = input()
+    clear_delay(1.5)
+
+    if customer_is_new:
+        print("Let's open your first account, {}".format(name))
+        clear_delay(1.5)
+    else:
+        print("Welcome back, {}".format(name))
+        clear_delay(1.5)
 
 main()

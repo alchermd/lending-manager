1446d247d6eaf2b6d9e2d12c8bde34be4b8eca7e
alchermd <johnalcherdoloiras@gmail.com>
2017-05-05 11:13:52 +0800

    Implement account creation option.
    Entering "0" when prompted allows the customer to create an account and borrow money. A confirmation prompt also appears to remind the customer of the interest rate on their account.
    
    The message printed when a new customer registers is changed to 2 seconds delay.

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     if customer_is_new:
         database[name] = Borrower(name)
         print("You are now registered to our services, {}".format(name))
-        clear_delay(1)
+        clear_delay(2)
     else:
         print("Welcome back, {}".format(name))
         clear_delay(1)
@@ -52,6 +52,29 @@ def main():
 
         choice = input().upper()
         clear_delay(1)
+
+        # Account creation.
+        if choice == '0':
+            # Prompt for amount.
+            print("How much will you borrow?")
+            print("Amount: ", end='')
+            amt = int(input())
+            clear_delay(1)
+
+            # Ask for a confirmation.
+            print("A 5% interest rate will be applied weekly on this account.")
+            print("Enter \"YES\" to confirm: ", end='')
+            agree = input().upper() == "YES"
+            clear_delay(1)
+
+            if agree:
+                database[name].open_account(amt, date.today(), 5)
+                print("Account created! Summary: ")
+                database[name].show_credits(-1)
+                clear_delay(5)
+
+            else:
+                continue
 
     print("Thank you for using our services. See you soon!")
 
